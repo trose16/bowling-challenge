13659b63e996e3a6d29e99fcb03b04ee8060c067
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-19 09:56:26 +0000

    Test planning

---
+++ spec/BowlingChallengeSpec.js
@@ -0,0 +1,30 @@
+describe ("bowlingChallenge", function(){
+
+  var bowlingGame
+
+  beforeEach(function(){
+    bowlingGame = new bowlingGame
+  });
+
+  describe("it has a scorecard that calculates points", function() {
+    it("can score a gutter ball", function(){
+      // add test here
+    });
+    it("can score single points", function(){
+      // add test here
+    });
+    it("can score single points", function(){
+      // add test here
+    });
+    it("can score a spare", function(){
+      // add test here
+    });
+    it("can score a strike", function(){
+      // add test here
+    });
+
+  });
+
+
+
+});

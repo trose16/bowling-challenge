0718f18061bc4cf76b6ad09ea44259d5d6ea1800
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-19 11:52:20 +0000

    First test ensures 10 frames

--- spec/BowlingChallengeSpec.js
+++ spec/BowlingChallengeSpec.js
@@ -1,30 +1,13 @@
-describe ("bowlingChallenge", function(){
+describe ("bowlingChallenge", function() {
 
-  var bowlingGame
+  var game;
 
   beforeEach(function(){
-    bowlingGame = new bowlingGame
+    game = new BowlingGame;
   });
 
-  describe("it has a scorecard that calculates points", function() {
-    it("can score a gutter ball", function(){
-      // add test here
-    });
-    it("can score single points", function(){
-      // add test here
-    });
-    it("can score single points", function(){
-      // add test here
-    });
-    it("can score a spare", function(){
-      // add test here
-    });
-    it("can score a strike", function(){
-      // add test here
-    });
-
+  it('has a total of 10 frames', function(){
+    expect(game.frames).toEqual(10)
   });
-
-
 
 });
--- src/BowlingChallenge.js
+++ src/BowlingChallenge.js
@@ -1,5 +1,5 @@
-'use strict';
 
-function bowlingGame() {
 
+function BowlingGame() {
+  this.frames = 10;
 };

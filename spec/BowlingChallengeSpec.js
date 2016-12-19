9c4f8557c4a1ceb27cf99d2080d3f35fe174077c
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-19 14:54:53 +0000

    Throws an error if player tries to play more than 10 frames

--- spec/BowlingChallengeSpec.js
+++ spec/BowlingChallengeSpec.js
@@ -14,18 +14,44 @@ describe ("bowlingChallenge", function() {
     expect(game.currentFrame()).toEqual(1)
   });
 
+  it('throws an error if you try to play more than 10 frames', function() {
+    for(frame = 0; frame < 10; frame++) { game.currentFrame() }
+    expect(function() { game.currentFrame()}).toThrowError("You've played 10 frames, start a new game!")
+  });
+
+
+  it('allows you to roll up to two times per frame', function() {
+
+  });
+
   it('has a scorecard to keep track of points', function() {
     expect(game.score).toEqual(0)
   });
 
+
   it("can score a gutter ball", function() {
-    game.roll(0)
+    game.roll(1, 0)
     expect(game.score).toEqual(0)
   });
 
   it('can score single points', function() {
-    game.roll(6)
+    game.roll(1, 3)
+    game.roll(1, 3)
     expect(game.score).toEqual(6)
+  });
+
+  it('can score a spare', function() {
+    game.roll(1, 6)
+    game.roll(2, 4)
+    game.roll(1, 2)
+    expect(game.score).toEqual(12)
+  });
+
+  it('can score a strike', function() {
+    game.roll(1, 10)
+    game.roll(1, 1)
+    game.roll(2, 8)
+    expect(game.score).toEqual(19)
   });
 
 });
--- src/BowlingChallenge.js
+++ src/BowlingChallenge.js
@@ -1,3 +1,4 @@
+DEFAULT_PINS = 10;
 
 function BowlingGame() {
   this.frames = 10;
@@ -7,9 +8,13 @@ function BowlingGame() {
 };
 
 BowlingGame.prototype.currentFrame = function(){
-  return this.frameCounter
+  if(this.frameCounter > 10){
+    throw new Error("You've played 10 frames, start a new game!")
+  }
+  this.frameCounter++
 };
 
-BowlingGame.prototype.roll = function(pinsHit){
+BowlingGame.prototype.roll = function(times, pinsHit){
   this.score += pinsHit;
+// this fx needs to allow 2 rolls unless strike ==10 and add one to the frame counter. Frame counter should stop at 10 and provide final score.
 };

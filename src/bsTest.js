f8d3c3cdfe2aba70689821a0960f4dac49078664
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-21 14:57:49 +0000

    Score method refactor extracts spare logic to new  method in test doc

--- src/bsTest.js
+++ src/bsTest.js
@@ -10,8 +10,8 @@ BowlingGame.prototype.score = function() {
   let score = 0;
   let i = 0;
   for (var frame = 0; frame < 10; frame++) {
-    // line below: if the roll1(i) in the rolls list + the roll2(i+1) in the rolls list is equal to 10, then add 10 to the score + the amount of the roll3(i+2) in the rolls list. i+=2 ensures the new total includes the bonus and the third roll. 
-      if(this.rolls[i] + this.rolls[i+1] === 10){
+    // line below: if the roll1(i) in the rolls list + the roll2(i+1) in the rolls list is equal to 10, then add 10 to the score + the amount of the roll3(i+2) in the rolls list. i+=2 ensures the new total includes the bonus and the third roll.
+      if(this.isSpare(i)){
         score += 10 + this.rolls[i+2];
         i+=2;
       } else {
@@ -20,4 +20,8 @@ BowlingGame.prototype.score = function() {
       }
   }
   return score;
+};
+
+BowlingGame.prototype.isSpare = function(i) {
+  return this.rolls[i] + this.rolls[i+1] === 10;
 };

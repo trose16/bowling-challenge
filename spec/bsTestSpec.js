3d864b82cb227539fde80a3e9b941990064644df
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-21 15:06:42 +0000

    Strike bonus points work on test docs

--- spec/bsTestSpec.js
+++ spec/bsTestSpec.js
@@ -14,6 +14,14 @@ describe ("bowlingChallenge", function() {
     expect(game.score()).toEqual(16);
   });
 
+  it('can score a strike', function() {
+    game.roll(10);
+    game.roll(1);
+    game.roll(1);
+    roll(17, 0);
+    expect(game.score()).toEqual(14);
+  });
+
   function roll(times, pinsDown) {
     for (var i = 0; i < times; i++) {
       game.roll(pinsDown);
--- src/bsTest.js
+++ src/bsTest.js
@@ -10,10 +10,12 @@ BowlingGame.prototype.score = function() {
   let score = 0;
   let i = 0;
   for (var frame = 0; frame < 10; frame++) {
-    // line below: if the roll1(i) in the rolls list + the roll2(i+1) in the rolls list is equal to 10, then add 10 to the score + the amount of the roll3(i+2) in the rolls list. i+=2 ensures the new total includes the bonus and the third roll.
-      if(this.isSpare(i)){
-        score += 10 + this.rolls[i+2];
-        i+=2;
+      if (this.isStrike(i)) {
+          score += 10 + this.rolls[i+1] + this.rolls[i+2];
+          i++;
+      } else if (this.isSpare(i)){
+          score += 10 + this.rolls[i+2];
+          i+=2;
       } else {
           score += this.rolls[i] + this.rolls[i+1];
           i+=2;
@@ -24,4 +26,8 @@ BowlingGame.prototype.score = function() {
 
 BowlingGame.prototype.isSpare = function(i) {
   return this.rolls[i] + this.rolls[i+1] === 10;
+};
+
+BowlingGame.prototype.isStrike = function(i) {
+  return this.rolls[i] === 10;
 };

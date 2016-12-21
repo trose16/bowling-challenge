f0bf1fcc738a9451622638f201aeb7cbb2fb1740
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-21 15:30:36 +0000

    Can score perfect game in test doc

--- spec/bsTestSpec.js
+++ spec/bsTestSpec.js
@@ -22,6 +22,11 @@ describe ("bowlingChallenge", function() {
     expect(game.score()).toEqual(14);
   });
 
+  it('can score a perfect game', function() {
+    game.rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    expect(game.score()).toEqual(300);
+  });
+
   function roll(times, pinsDown) {
     for (var i = 0; i < times; i++) {
       game.roll(pinsDown);
